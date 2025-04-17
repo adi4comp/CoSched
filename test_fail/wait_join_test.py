6227f766725ed1618f57513c659d96c3bb892169
adi4comp <adi4workspace@gmail.com>
2025-04-18 00:09:38 +0800

    add wait join test and some fix in deadlock detection

---
+++ test_fail/wait_join_test.py
@@ -0,0 +1,32 @@
+import cosched as threading
+from cosched import *
+import time
+import sys
+
+lock_1 = Lock()
+def task(i):
+    pass
+
+def task_2(thread):
+    lock_1.acquire()
+    thread.join()
+
+if __name__ == "__main__":
+    if "--verbose" in sys.argv:
+        cosched_set_verbose()
+    if "--interactive" in sys.argv:
+        print("Interactive policy selected")
+        cosched_set_policy(0)
+    elif "--priority" in sys.argv:
+        print("Priority policy selected")
+        cosched_set_policy(2)
+    else:
+        print("Random policy selected")
+        cosched_set_policy(1)
+    
+    
+    threads = [Thread(target=task, args=(i+1,)) for i in range(2)]
+    threads.append(Thread(target=task_2, args=(threads[0],)))
+    threads.append(Thread(target=task_2, args=(threads[2],)))
+    
+    cosched_start()
