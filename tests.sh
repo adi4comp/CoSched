7db6633f27dd159afeb42a8e4dc0c763d8f295df
adi4comp <adi4workspace@gmail.com>
2025-04-18 20:52:01 +0800

    modify test scripts and store logs

--- tests.sh
+++ tests.sh
@@ -4,11 +4,26 @@ source env.sh
 
 
 cd test_fail
+rm -rf logs
+mkdir -p logs
+
+iterations=$1
+if [ -z "$iterations" ]; then
+    iterations=1
+fi
 
 for test_file in *.py; do
     if [ -f "$test_file" ]; then
-        echo "Running test: $test_file"
-        python3 "$test_file"
-        echo "--------------------------------------------------"
+    echo "-----------------------------------------------------------------------------"
+    echo "Running test: $test_file"
+    for ((i=1; i<=iterations; i++)); do
+        echo "" >> logs/${test_file}.log
+        echo "<=================================================================================>" >> logs/${test_file}.log
+        echo "Running test: $test_file, iteration: $i" >> logs/${test_file}.log
+        echo "Running test: $test_file, iteration: $i"
+        python3 "$test_file" >> logs/${test_file}.log 2>&1
+        
+    done
+    echo "<=================================================================================>" >> logs/${test_file}.log
     fi
 done
